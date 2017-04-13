015ef5a0f5805d3790810a481f730f17e6085739
rmskaff <ritamskaff@gmail.com>
2017-04-13 16:54:51 -0400

    First Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+alert ('hi');
+
+var fahrenheit=32;
+fahrenheit=((fahrenheit-32)*(5/9));
+  console.log("The temperature is " + fahrenheit + " degrees Celsius.");
+
+var celsius= 0;
+celsius=((celsius*9)/5)+32;
+console.log("The temperature is " + celsius + " degrees Fahrenheit.");
